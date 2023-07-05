c9c7cd45cba30495ba09689f0b72f3ac305d1f33
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-05 20:24:13 +0200

    first steps into setting up web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 # setup web static files
+sudo apt-get update > /dev/null
+sudo apt-get -y install nginx > /dev/null
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
+
+echo "test file" > sudo /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown ubuntu:ubuntu /data/ -hR
