12c5ea0c5e520e56b32e5bce8d27a740e1ec83d9
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-16 19:44:06 +0200

    Add the configuration part of the location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,3 +14,7 @@ echo "HBNB Static files" > sudo /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown ubuntu:ubuntu /data/ -hR
+
+sudo sed -i '22i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
