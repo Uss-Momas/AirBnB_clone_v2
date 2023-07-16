13a8db1b10c966c71f8f260e8598697820888166
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-16 20:01:54 +0200

    New config file for web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# setup web static files
+# Script to configre nginx server folder
 sudo apt-get update > /dev/null
 sudo apt-get -y install nginx > /dev/null
 
@@ -7,12 +7,9 @@ sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-cd /data/web_static/releases/test/ ; echo "HBNB Static files" > sudo index.html
+echo "test index html" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown ubuntu:ubuntu /data/ -hR
 
 sudo sed -i '22i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
