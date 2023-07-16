dc19a39d92dd776e763d3f4644a55e6f96d23fd5
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-16 20:21:38 +0200

    add condition to control dir creation"

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,16 +2,18 @@
 """pack web static module"""
 from fabric.api import *
 from datetime import datetime
+from os.path import isdir
 
 
 def do_pack():
     """generates a .tgz archive from contents of a folder"""
     try:
-        local("mkdir -p versions")
+        if isdir("versions") is False:
+            local("mkdir -p versions")
         time = datetime.now()
         date = time.strftime('%Y%m%d%H%M%S')
         file_path = 'versions/web_static_{}.tgz'.format(date)
-        local("tar -cvzf {} ./web_static".format(file_path))
+        local("tar -cvzf {} web_static".format(file_path))
         return file_path
     except Exception:
         return None
