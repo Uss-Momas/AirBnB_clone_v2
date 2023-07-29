ecbd2ec54315259854fce73a5b435205d24ef7a9
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-29 15:58:27 +0200

    flask optional parameter

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""hello route flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello world from application"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display only HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c(text):
+    """receive a param process and return it"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Optional parameter"""
+    return text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
