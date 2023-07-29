0474d91619dd13ed42acdf724b6c30aabdabf044
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-29 16:11:44 +0200

    flask accept int parameter on /number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""hello route flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello world from application"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display only HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c(text):
+    """receive a param process and return it"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Optional parameter"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """verify if n is integer"""
+    return "{} is a number".format(int(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
