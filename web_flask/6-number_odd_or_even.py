490ddda49ff9671254ebfaf883d7bc28a2fe306f
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-07-31 21:04:33 +0200

    verifying if a number is odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""hello route flask"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello world from application"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display only HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c(text):
+    """receive a param process and return it"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Optional parameter"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """verify if n is integer"""
+    return "{} is a number".format(int(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False, )
+def number_template(n):
+    """Jinja displaying the parameter"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_even(n):
+    """says that number is odd or even"""
+    return render_template("6-number_odd_or_even.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
